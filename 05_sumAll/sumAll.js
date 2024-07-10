b1120e58388b1615288ec0027825f0548f57d404
Kartikshukla17 <kartikconnect17@gmail.com>
2024-07-10 16:30:50 +0530

    sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    for (let sum = a ; sum <b+1 ; sum++){
+        sum += sum
+        return sum;
+    }
 };
 
 // Do not edit below this line
