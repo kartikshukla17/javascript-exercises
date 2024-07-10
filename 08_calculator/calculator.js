811eaafa06d1618383656420a7a04de0fd9edef9
Kartikshukla17 <kartikconnect17@gmail.com>
2024-07-10 16:55:38 +0530

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, Current) = total + Current,0);
 };
 
-const multiply = function() {
-
+const multiply = function(a,b) {
+  return a*b;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  }
+  let fact = 1
+	for (i =1; i< a; i++){
+    fact = fact * i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
