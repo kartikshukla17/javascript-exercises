6235473902a867fb5bf69c646e00c3bf7c3ce9ec
Kartikshukla17 <kartikconnect17@gmail.com>
2024-07-10 15:58:41 +0530

    reverseString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const { rankings } = require("match-sorter");
 
+const repeatString = function(word , num) {
+    if (num < 0)
+        return "Error";
+    let string = ""
+    for (let i = 0; i < num; i++){
+        string += word;
+    }
+    return string
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverseString().join("");
 };
+
+reverseString("hello")
 
 // Do not edit below this line
 module.exports = reverseString;
