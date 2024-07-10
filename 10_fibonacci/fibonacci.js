15966ea32a1805dbb3c40c68dd5217edd91576c7
Kartikshukla17 <kartikconnect17@gmail.com>
2024-07-10 23:54:40 +0530

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
 const fibonacci = function() {
-
+    let start = [0,1]
+    for (let i = 2; i < 100; i++) {
+        start.push(start[i-1] + start[i-2])
+        }
+    return start.slice(0,100)
 };
+
+fibonacci()
 
 // Do not edit below this line
 module.exports = fibonacci;
