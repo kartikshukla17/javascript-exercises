eea34b8ad95270607e9b3947b56eb7e4503b0248
Kartikshukla17 <kartikconnect17@gmail.com>
2024-07-11 02:05:35 +0530

    findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people((oldest,currentPerson) => {
+        if (currentPerson.age > oldest.age) {
+            return currentPerson
+            } else {
+                return oldest
+                }
+                })
+            }
 
-};
 
 // Do not edit below this line
 module.exports = findTheOldest;
